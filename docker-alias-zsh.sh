ec0a1e80a03791e8e2f20e1d206b83d6a0ca7ab4
zlld <lidzhu@gmail.com>
2016-06-24 15:47:14 +0800

    add docker alias

---
+++ docker-alias-zsh.sh
@@ -0,0 +1,24 @@
+
+alias drmi="docker rmi $1"
+alias drmif="docker rmi -f $1"
+alias drmn="docker rmi -f $(docker images | grep '<none>' | awk '{print $3}')"
+# Stop all containers
+dstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+drm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove all images
+dri() { docker rmi $(docker images -q); }
+
+# Dockerfile build, e.g., $dbu tcnksm/test 
+dbu() { docker build -t=$1 .; }
+
+# Show all alias related docker
+dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
+
+# Bash into running container
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash }
